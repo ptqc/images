b24e80be241cd7717dd2495feecbc233e012cfea
Simon <shuang@coremail.cn>
2019-03-14 16:30:25 +0800

    ICM-1376 CAC3.0-CAC云管理中心-前端功能开发

--- docker/cloud/_root/home/mailtech/bin/image-init.sh
+++ docker/cloud/_root/home/mailtech/bin/image-init.sh
@@ -3,11 +3,22 @@
 BIN_DIR=$(dirname "$0")
 
 
-installMySQLForPython() {
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py
-    pip install mysql.connector
+prepare() {
+    apt-get update && apt-get install -y vim
 }
 
-installMySQLForPython
+installMySQLForPython() {
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py && pip install mysql.connector
+}
+
+installCrontab() {
+    apt-get -y install cron
+}
+
+run() {
+    prepare && installMySQLForPython && installCrontab
+}
+
+
+run
 
